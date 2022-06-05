67b7f1017d0cd9ad18b67b45b5b7b3fae706bdb1
Aleksandrov Aleksander <aaleksandrov@dev.rtsoft.ru>
2022-06-05 17:51:11 +0300

    add installation feature

--- example/include/basic_handler.hpp
+++ example/include/basic_handler.hpp
@@ -1,9 +1,22 @@
 #include <iostream>
 #include <fstream>
+#include <sys/stat.h>
 
 #include "ddi.hpp"
 
 using namespace ddi;
+
+char *getEnvOrExit(const char *name) {
+    auto env = std::getenv(name);
+    if (env == nullptr) {
+        std::cout << "Environment variable " << name << " not set" << std::endl;
+        exit(2);
+    }
+    return env;
+}
+
+const auto INSTALLATION_SCRIPT = std::string(getEnvOrExit("INSTALLATION_SCRIPT"));
+
 
 class CancelActionFeedbackDeliveryListener : public ResponseDeliveryListener {
 public:
@@ -27,6 +40,58 @@ public:
     }
 };
 
+struct CommandResult {
+    std::string output;
+    int exitstatus;
+
+    friend std::ostream &operator<<(std::ostream &os, const CommandResult &result) {
+        os << "command exitstatus: " << result.exitstatus << " output: " << result.output;
+        return os;
+    }
+    bool operator==(const CommandResult &rhs) const {
+        return output == rhs.output &&
+               exitstatus == rhs.exitstatus;
+    }
+    bool operator!=(const CommandResult &rhs) const {
+        return !(rhs == *this);
+    }
+};
+
+
+class Command {
+
+public:
+    static CommandResult exec(const std::string &command) {
+        int exitcode = 255;
+        std::array<char, 1048576> buffer {};
+        std::string result;
+#ifdef _WIN32
+        #define popen _popen
+#define pclose _pclose
+#define WEXITSTATUS
+#endif
+        FILE *pipe = popen(command.c_str(), "r");
+        if (pipe == nullptr) {
+            throw std::runtime_error("popen() failed!");
+        }
+        try {
+            std::size_t bytesread;
+            while ((bytesread = fread(buffer.data(), sizeof(buffer.at(0)), sizeof(buffer), pipe)) != 0) {
+                auto res = std::string(buffer.data(), bytesread);
+                result += res;
+                std::cout << res;
+            }
+        } catch (...) {
+            pclose(pipe);
+            throw;
+        }
+        std::cout << std::endl;
+        exitcode = WEXITSTATUS(pclose(pipe));
+        return CommandResult{result, exitcode};
+    }
+};
+
+
 class Handler : public EventHandler {
 public:
     std::unique_ptr<ConfigResponse> onConfigRequest() override {
@@ -41,42 +106,66 @@ public:
                 ->build();
     }
 
-    std::unique_ptr<Response> onDeploymentAction(std::unique_ptr<DeploymentBase> dp) override {
+    std::unique_ptr<Response> processInstall(std::unique_ptr<DeploymentBase> dp) {
         std::cout << ">> Get Deployment base request" << std::endl;
         std::cout << " id: " << dp->getId() << " update: " << dp->getUpdateType() << std::endl;
         std::cout << " download: " << dp->getDownloadType() << " inWindow: " << (bool) dp->isInMaintenanceWindow()
                   << std::endl;
-
         auto builder = ResponseBuilder::newInstance();
-        builder->addDetail("Printed deployment base info");
         std::cout << " + CHUNKS:" << std::endl;
 
         for (const auto &chunk: dp->getChunks()) {
             std::cout << "  part: " << chunk->getPart() << std::endl;
             std::cout << "  name: " << chunk->getName() << " version: " << chunk->getVersion() << std::endl;
+
+            auto chunkPath = chunk->getVersion() + "@" + chunk->getName();
+            if (mkdir(chunkPath.c_str(), 0744) != 0)
+                throw std::runtime_error("Cannot create directory: " + chunkPath);
+
             std::cout << "  + ARTIFACTS:" << std::endl;
             for (const auto &artifact: chunk->getArtifacts()) {
                 std::cout << "   filename: " << artifact->getFilename() << " size: " << artifact->size() << std::endl;
                 std::cout << "   md5: " << artifact->getFileHashes().md5 << std::endl;
                 std::cout << "   sha1: " << artifact->getFileHashes().sha1 << std::endl;
                 std::cout << "   sha256: " << artifact->getFileHashes().sha256 << std::endl;
-                builder->addDetail(artifact->getFilename() + " described. Starting download ...");
-                std::cout << "  .. downloading " + artifact->getFilename() + "...";
-                artifact->downloadTo(artifact->getFilename());
-                builder->addDetail("Downloaded " + artifact->getFilename());
+                auto artifactPath =  chunkPath + "/" + artifact->getFilename();
+
+                std::cout << "  .. downloading " + artifactPath + "...";
+                artifact->downloadTo(artifactPath);
                 std::cout << "[OK]" << std::endl;
             }
             std::cout << " + ---------------------------" << std::endl;
         }
 
-        return builder->addDetail("Work done. Sending response")
+
+        auto output = Command::exec("python3 " + INSTALLATION_SCRIPT + " .");
+
+        auto finished = (output.exitstatus == 0) ? Response::SUCCESS : Response::FAILURE;
+
+        return builder->addDetail(output.output)
                 ->setIgnoreSleep()
                 ->setExecution(Response::CLOSED)
-                ->setFinished(Response::SUCCESS)
+                ->setFinished(finished)
                 ->setResponseDeliveryListener(
                         std::shared_ptr<ResponseDeliveryListener>(new DeploymentBaseFeedbackDeliveryListener()))
                 ->build();
+    }
 
+    std::unique_ptr<Response> onDeploymentAction(std::unique_ptr<DeploymentBase> dp) override {
+        try {
+            return processInstall(std::move(dp));
+        } catch (std::exception &e) {
+            auto builder = ResponseBuilder::newInstance();
+            return builder->addDetail("Got exception at installing package")
+                    ->setIgnoreSleep()
+                    ->addDetail("Exception:")
+                    ->addDetail(e.what())
+                    ->setExecution(Response::CLOSED)
+                    ->setFinished(Response::FAILURE)
+                    ->setResponseDeliveryListener(
+                            std::shared_ptr<ResponseDeliveryListener>(new DeploymentBaseFeedbackDeliveryListener()))
+                    ->build();
+        }
     }
 
     std::unique_ptr<Response> onCancelAction(std::unique_ptr<CancelAction> action) override {
@@ -102,12 +191,3 @@ public:
 
     Handler() = default;
 };
-
-char *getEnvOrExit(const char *name) {
-    auto env = std::getenv(name);
-    if (env == nullptr) {
-        std::cout << "Environment variable " << name << " not set" << std::endl;
-        exit(2);
-    }
-    return env;
-}
